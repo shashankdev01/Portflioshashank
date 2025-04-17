b42c4ad019d257890a57c14e1f3c6f3eeaa4d715
shashank singh <shashank12102001@gmail.com>
2025-04-17 15:22:00 +0530

    second commit

--- shashankPortfolio/src/Sections/Cta.jsx
+++ shashankPortfolio/src/Sections/Cta.jsx
@@ -16,7 +16,7 @@ useEffect( () => {
   return (
     <div className='w-full lg:px-[200px] px-8 py-[60px] h-full flex flex-col justify-between itmes-center gap-5'>
        <div data-aos='zoom-in' className='w-full bg-[#2f2f2f] flex lg:flex-row flex-col justify-between items-center lg:p-20 p-8'>
-        <h1 className='text-white lg:w-[50%] w-full lg:text-[35px] leading-10 lg:text-left text-center font-semibold font-poppins'>Discover how design can help your  bsiness</h1>
+        <h1 className='text-white lg:w-[50%] w-full lg:text-[35px] leading-10 lg:text-left text-center font-semibold font-poppins'>Discover how design can help your business</h1>
         <a href='mailto:shashanksingh.dev01@gmail.com'><button className='bg-themered hover:bg-white tex-white hover:text-themered font-semibold lg:text-sm px-8 py-3 rounded-lg mt-5 font-poppins '>Contact Me</button></a> 
        </div>
     </div>
